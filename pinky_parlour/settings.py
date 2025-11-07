7e3acb63f5710abc9f8ff05adf547fe59c2f7291
jes <jajesma05@gmail.com>
2025-11-07 12:45:14 +0530

    Django Railway setup fixes

--- pinky_parlour/settings.py
+++ pinky_parlour/settings.py
@@ -143,6 +143,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / 'main' / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
